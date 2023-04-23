f456e403a664e6cdb818e8d19aa3b93bdd8f298a
Frameluffy <hackermaster.everybody@gmail.com>
2023-04-23 22:25:34 +0700

    edit plot

--- plot.py
+++ plot.py
@@ -14,9 +14,10 @@ def plot_image_withColor(img_tensor, annotation):
     # Display the image
     ax.imshow(np.transpose(img,(1,2,0)))
         
-    for (box, label) in zip(annotation["boxes"],annotation["labels"]):
+    for (box, label, scores) in zip(annotation["boxes"],annotation["labels"],annotation["scores"]):
         img = img_tensor.cpu().data.numpy()
-
+        score = np.round(scores.cpu().numpy(),3)
+        print(score)
         # Display the image
         # ax.imshow(np.transpose(img,(1,2,0)))
         xmin, ymin, xmax, ymax = box.cpu()
@@ -24,23 +25,23 @@ def plot_image_withColor(img_tensor, annotation):
         # Create a Rectangle patch with different colors
         #red: with mask  green: mask_weared_incorrect  blue: without mask
             rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='g',facecolor='none')
-            text = "With mask"
-            color = "#8fce00"
+            text = f"With mask \nScore : {str(score)}"
+            color = "green"
         elif(label == 2):
             rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='r',facecolor='none')
-            text = "Without mask"
-            color = "#f44336"
+            text = f"Without mask \nScore : {str(score)}"
+            color = "red"
             print(st.session_state['playsound'])
             if st.session_state['playsound']%30 == 0:
                 alert()
             st.session_state['playsound'] += 1
-        else:
+        elif(label == 3):
             rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='b',facecolor='none')
-            text = "Mask weared incorrect"
-            color = "#2986cc"
+            text = f"Mask weared incorrect \nScore : {str(score)}"
+            color = "blue"
             
         ax.add_patch(rect)
-        ax.text(xmin+10, ymin+20, text,fontfamily="sans-serif",color=color)
+        ax.text(xmin+5, ymin-10, text,fontfamily="sans-serif",color="white",backgroundcolor=color)
     
     plt.axis("off")   
     
